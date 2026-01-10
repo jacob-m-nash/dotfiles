cbe1b98f09abf22787a246eb7aa0479a3b399dfe
jacob-m-nash <jacob.matthew.nash@gmail.com>
2026-01-10 19:45:40 +0000

    better xarco/xml lsp

--- nvim/lua/plugins/xml.lua
+++ nvim/lua/plugins/xml.lua
@@ -3,9 +3,26 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        lemminx = {},
+        lemminx = {
+          settings = {
+            xml = {
+              validation = {
+                noGrammar = "ignore",
+              },
+            },
+          },
+        },
       },
     },
+    init = function()
+      vim.filetype.add({
+        extension = {
+          xacro = "xml",
+          urdf = "xml",
+          sdf = "xml",
+        },
+      })
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
